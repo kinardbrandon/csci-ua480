51cc5cc631b56820646e3f164dc956efc0443499
josiewilliams13 <35999827+josiewilliams13@users.noreply.github.com>
2018-06-07 17:38:01 -0400

    Josie Williams - Assignment 1 (#38)
    * Examples from class 5/31/18
    
    * moved my scratch code and scenes into dag dir
    
    * changed meta file
    
    * In-class examples from 5/31
    
    * remove cruft from master
    
    * minor changes to example scenes
    
    * added mac os .DS_Store files to gitignore
    
    * added unity character assets and google cardboard
    
    * fix FPSController running in Editor with Build Settings set to iOS
    
    * raycasting example for assignment 2 (#36)
    
    * assignment_01

---
+++ Assets/Assignments/Assignment_01/A01_jvw242/Scripts/Moon1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moon : MonoBehaviour
+{
+
+    public float speed = 3.0f;
+    Vector3 parentPos = new Vector3(0, 0, 0);
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * speed);
+
+    }
+}
