44d7c00f0ad8d218d62b6a0c635268ae9a407583
Zhongheng Sun <zhonghengsun@Zhonghengs-Air.fios-router.home>
2018-06-12 02:09:49 -0400

    plz work

---
+++ Assets/Assignments/Assignment_02/A02_zs967/Scripts/FusRohDah.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FusRohDah : MonoBehaviour
+{
+
+    public AudioClip frd;
+    public Camera Camera;
+    public GameObject cube;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
+        bool down = Input.GetKey(KeyCode.F);
+        if (Physics.Raycast(ray, out hit)){
+            Transform objectHit = hit.transform;
+            if (down)
+            {
+                cube.GetComponent<Rigidbody>().AddForce(Camera.transform.forward * 5000);
+                if (frd != null){
+                    AudioSource.PlayClipAtPoint(frd, Camera.main.transform.position);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Assignments/Assignment_02/A02_zs967/Scripts/Shooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace A02_zs967{
+    public class Shooting : MonoBehaviour
+    {
+
+        GameObject prefab;
+        Rigidbody rb;
+        // Use this for initialization
+        void Start()
+        {
+            prefab = Resources.Load("bullet") as GameObject;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButton(0)){
+                GameObject bullet = Instantiate(prefab) as GameObject;
+                bullet.transform.position = transform.position + Camera.main.transform.forward * 2;
+                rb = bullet.GetComponent<Rigidbody>();
+                rb.velocity = Camera.main.transform.forward * 40;
+
+            }
+        }
+    }
+}
