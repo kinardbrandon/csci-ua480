e3db527cc0918cb88bf6832534029d872d9a9792
josiewilliams13 <35999827+josiewilliams13@users.noreply.github.com>
2018-06-14 03:30:57 -0400

    Josie Williams - Assignment 02 (#61)
    * assignment_01
    
    * assignment 2
    
    * assignment)2

---
+++ Assets/Assignments/Assignment_01/A01_jvw242/Scripts/Orbits.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbits : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.down * Time.deltaTime * speed);
+
+    }
+
+}
---
+++ Assets/Control.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    /*Code is taken from Assignment 2 Scripts*/
+
+    /*****
+     * BallController
+     * Lets user push an object around using keyboard input
+     * Based on the Unity Roll-A-Ball Tutorial
+     *****/
+    public class Control : MonoBehaviour
+    {
+        // A multiplier to determine how strongly to push the ball
+        //  with each key press
+        public float speed = 10.0f;
+
+        Rigidbody player;
+
+        // Use this for initialization
+        void Start()
+        {
+            // Get my rigidbody
+            player = GetComponent<Rigidbody>();
+        }
+
+        // FixedUpdate is called once per physics update (a fixed timestep)
+        void FixedUpdate()
+        {
+            // Check if any of the keys are pressed for the 
+            // Horizontal or Vertical axes
+            float moveX = Input.GetAxis("Horizontal");
+            float moveZ = Input.GetAxis("Vertical");
+
+            // Use the input to compute a new force direction
+            Vector3 dir = new Vector3(moveX, 0.0f, moveZ);
+
+            // Push the ball in the direction specified
+            player.AddForce(dir * speed);
+        }
+    }
